3e396ee655c20b423d5181da8e81dc67ff5d1e94
John Kim <kimjon0509@gmail.com>
2020-06-25 14:52:04 +0000

    added table to admin/dashboard page

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,9 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['USER_NAME'], password: ENV['USER_PASSWORD'], only: :show
   def show
+    # Display a count of how many products are in the database
+    @products_count = Product.all.count
+    # Display a count of how many categories are in the database
+    @categories_count = Category.all.count
   end
 end
