c7a436c1356313a1b9a00fc4c2b471542da3e3b6
John Kim <kimjon0509@gmail.com>
2020-06-30 21:21:06 +0000

    fixed user authentication bug

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,24 @@ class User < ActiveRecord::Base
   validates :password, presence: true, length: { minimum: 6 }
   validates :password_confirmation, presence: true
 
+  before_validation(on: :create) do
+    self.email = format_email(email)
+  end
+
+  def format_email(email)
+    remove_white = email.strip unless self.email.nil?
+    email_arr = remove_white.split('')
+    email_arr.map! do |char|
+      if char.count("a-zA-Z") > 0
+        char.downcase
+      else
+        char
+      end
+    end
+    modified_email = email_arr.join
+    self.email = modified_email
+  end
+
   def self.authenticate_with_credentials(email, password)
     #remove whitespace and convert string to lowercase
     white_space_removed_email = email.strip
